766977a5412796beb8b6ed1f5f97382fc7047f13
Steveen <steveen.echeverri@gmail.com>
2020-06-28 19:56:49 -0500

    Execute a SQL query to create a new Artist

--- api/api.js
+++ api/api.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const apiRouter = express.Router();
-const artistRouter = require('./artists.js');
+const artistsRouter = require('./artists')
 
-apiRouter.use('/artist', artistRouter);
+apiRouter.use('/artists', artistsRouter);
 
 module.exports = apiRouter;
--- api/artists.js
+++ api/artists.js
@@ -5,12 +5,12 @@ const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
 artistsRouter.param('artistId', (req, res, next, artistId) => {
-    const sql = 'SELECT * FROM Artist WHERE ArtistId = $artistId';
-    const values = {$artistId: artistId}
+    const sql = 'SELECT * FROM Artist WHERE Artist.id = $artistId';
+    const values = { $artistId: artistId }
     db.get(sql, values, (error, artist) => {
-        if(error) {
+        if (error) {
             next(error);
-        } else if(artist){
+        } else if (artist) {
             req.artist = artist;
             next();
         } else {
@@ -20,7 +20,7 @@ artistsRouter.param('artistId', (req, res, next, artistId) => {
 });
 
 artistsRouter.get('/', (req, res, next) => {
-    db.all('SELECT * FROM Artits WHERE Artist.is_currently_employed = 1',
+    db.all('SELECT * FROM Artist WHERE Artist.is_currently_employed = 1',
         (err, artists) => {
             if (err) {
                 next(err);
@@ -28,6 +28,41 @@ artistsRouter.get('/', (req, res, next) => {
                 res.status(200).json({ artists: artists });
             }
         });
+})
+
+artistsRouter.get('/:artistId', (req, res, next) => {
+    res.status(200).json({ artist: req.artist });
+})
+
+artistsRouter.post('/', (req, res, next) => {
+    const name = req.body.artist.name;
+    const dateOfBirth = req.body.artist.dateOfBirth;
+    const biography = req.body.artist.biography;
+    const isCurrentlyEmployed = req.body.artist.isCurrentlyEmployed === 0 ? 0 : 1;
+
+    if (!name || !dateOfBirth || biography) {
+        return res.sendStatus(400);
+    }
+
+    const sql = 'INSERT INTO Artist (name, date_of_birth, biography, is_currently_employed)' + 
+                 'VALUES ($name, $dateOfBirth, $biography, $isCurrentlyEmployed)';
+    const values = {
+        $name: name,
+        $dateOfBirth: dateOfBirth,
+        $biography: biography,
+        $isCurrentlyEmployed: isCurrentlyEmployed
+    };
+
+    db.run(sql, values, function(error) {
+        if(error) {
+            next(error);
+        } else {
+            db.get(`SELECT * FROM Artist WHERE Artist.id = ${this.lastID}`,
+            (error, artist) => {
+                res.status(201).json({ artist: artist});
+            });
+        }
+    })
 })
 
 module.exports = artistsRouter; 
