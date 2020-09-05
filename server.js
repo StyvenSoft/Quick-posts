bfff97549c6135c1a5e213d2800347cccc8eeea0
Steveen <steveen.echeverri@gmail.com>
2020-09-05 16:34:57 -0500

    Setup server sqlite show homepage

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ const cors = require('cors');
 const errorhandler = require('errorhandler');
 const express = require('express');
 const morgan = require('morgan');
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
+
 
 const apiRouter = require('./api/api');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-
+app.use(express.static('./'));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(morgan('dev'));
