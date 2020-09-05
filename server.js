e0fa6187c47c63f06027acea67b8e7c8e30889c0
Steveen <steveen.echeverri@gmail.com>
2020-09-05 17:11:28 -0500

    new test database err logo

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const errorhandler = require('errorhandler');
 const express = require('express');
 const morgan = require('morgan');
 const sqlite3 = require('sqlite3');
-const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
+const db = new sqlite3.Database('./database.sqlite');
 
 
 const apiRouter = require('./api/api');
@@ -22,7 +22,7 @@ app.use(errorhandler());
 
 
 app.listen(PORT, () => {
-    console.log(`Listening on port: http://localhost/${PORT}`);
+    console.log(`Listening on port: http://localhost:${PORT}`);
 });
 
 module.exports = app;
